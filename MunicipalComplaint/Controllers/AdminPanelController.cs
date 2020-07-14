91953e11fa5b610683ec39c373d0e9676e57806c
hamza <hamza66789.mh@gmail.com>
2020-07-14 20:31:10 +0500

    Area With manage

--- MunicipalComplaint/Controllers/AdminPanelController.cs
+++ MunicipalComplaint/Controllers/AdminPanelController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.ComponentModel.DataAnnotations;
 using MunicipalComplaint.Models;
 using MunicipalComplaint.ViewModel;
+using System.Data.Entity;
+
 namespace MunicipalComplaint.Controllers
 {
     public class AdminPanelController : Controller
@@ -116,10 +118,118 @@ namespace MunicipalComplaint.Controllers
             return Json(tehsi);
 
         }
-        [HttpGet]
-        public ActionResult chpProvince(int id) {
+        [HttpPost]
+        public ActionResult chkProForUp(int id) {
             Province p = _context.province.Single(x=>x.ProvinceId==id);
             return Json(p);
+        }
+
+        [HttpPost]
+        public ActionResult chkDisForUp(int id)
+        {
+            City p = _context.city.Single(x => x.DistrictId == id);
+            return Json(p);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult UpdateDistrict(City ct)
+        {
+
+            var upDis = _context.city.Single(m => m.DistrictId == ct.DistrictId);
+            TryUpdateModel(upDis);
+            _context.SaveChanges();
+            return Json("done");
+        }
+        [HttpPost]
+        public ActionResult chkTehForUp(int id)
+        {
+            Tehsil p = _context.tehsil.Single(x => x.TehsilId == id);
+            return Json(p);
+        }
+        [HttpPost]
+        public ActionResult chkUcForUp(int id)
+        {
+            UC uc = _context.uc.Single(x => x.UcId == id);
+            return Json(uc);
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateTehsil(Tehsil teh)
+        {
+
+            var upTeh = _context.tehsil.Single(m => m.TehsilId == teh.TehsilId);
+            TryUpdateModel(upTeh);
+            _context.SaveChanges();
+            return Json("done");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult UpdateUc(UC uc)
+        {
+
+            var upUc = _context.uc.Single(m => m.UcId == uc.UcId);
+            TryUpdateModel(upUc);
+            _context.SaveChanges();
+            return Json("done");
+        }
+        [HttpPost]
+        public ActionResult updateProvince(Province pro) {
+
+          var upPro = _context.province.Single(m => m.ProvinceId== pro.ProvinceId);
+            TryUpdateModel(upPro);
+            _context.SaveChanges();
+            return Json("done");
+        }
+        [HttpPost]
+
+        public ActionResult DelProvince(int id) {
+            Province pro = _context.province.SingleOrDefault(x=>x.ProvinceId==id);
+            _context.province.Remove(pro);
+            _context.SaveChanges();
+                return Json("done");
+          
+           
+        }
+
+        [HttpPost]
+
+        public ActionResult DelDistrict(int id)
+        {
+            City city= _context.city.SingleOrDefault(x => x.DistrictId == id);
+            _context.city.Remove(city);
+            _context.SaveChanges();
+            return Json("done");
+
+
+        }
+
+        [HttpPost]
+
+        public ActionResult DelTehsil(int id)
+        {
+            Tehsil teh= _context.tehsil.SingleOrDefault(x => x.TehsilId == id);
+            _context.tehsil.Remove(teh);
+            _context.SaveChanges();
+            return Json("done");
+
+
+        }
+
+        [HttpPost]
+
+        public ActionResult DelUc(int id)
+        {
+            UC ucc= _context.uc.SingleOrDefault(x => x.UcId == id);
+            _context.uc.Remove(ucc);
+            _context.SaveChanges();
+            return Json("done");
+
+
         }
 
         protected override void Dispose(bool disposing)
---
+++ MunicipalComplaint/Scripts/AreasAjax/AreaDel.js
@@ -0,0 +1,159 @@
+﻿function deleteProvince(id) {
+    var id = id;
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted,  you'r all record related to Province will be delete!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "POST",
+                    url: "/AdminPanel/DelProvince",
+                    //data: data,
+                    data: { id: id },
+                    success: function (re) {
+                        swal({
+                            title: "Good job!",
+                            text: "Poof! Your Record has been deleted!!",
+                            icon: "success",
+                            button: true,
+
+                        }).then((value) => {
+
+                            location.reload();
+                        });
+                    }
+                });
+
+            } else {
+                swal("Your Record is safe!")
+            }
+        });
+}
+
+//District Delete
+
+function deleteDistrict(id) {
+    var id = id;
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted,  you'r all record related to District will be delete!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "POST",
+                    url: "/AdminPanel/DelDistrict",
+                    //data: data,
+                    data: { id: id },
+                    success: function (re) {
+                        swal({
+                            title: "Good job!",
+                            text: "Poof! Your Record has been deleted!!",
+                            icon: "success",
+                            button: true,
+
+                        }).then((value) => {
+
+                            location.reload();
+                        });
+                    }
+                });
+
+            } else {
+                swal("Your Record is safe!")
+            }
+        });
+
+
+}
+
+//Tehsil Delete
+
+function deleteTehsil(id) {
+    var id = id;
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted,  you'r all record related to Tehsil will be delete!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "POST",
+                    url: "/AdminPanel/DelTehsil",
+                    //data: data,
+                    data: { id: id },
+                    success: function (re) {
+                        swal({
+                            title: "Good job!",
+                            text: "Poof! Your Record has been deleted!!",
+                            icon: "success",
+                            button: true,
+
+                        }).then((value) => {
+
+                            location.reload();
+                        });
+                    }
+                });
+
+            } else {
+                swal("Your record is safe!")
+            }
+        });
+
+
+}
+
+//Delete Uc
+
+function deleteUc(id) {
+    var id = id;
+
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you'r all record related to Uc will be delete!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: "POST",
+                    url: "/AdminPanel/DelUc",
+                    //data: data,
+                    data: { id: id },
+                    success: function (re) {
+                        swal({
+                            title: "Good job!",
+                            text: "Poof! Your Record has been deleted!!",
+                            icon: "success",
+                            button: true,
+
+                        }).then((value) => {
+
+                            location.reload();
+                        });
+                    }
+                });
+
+            } else {
+                swal("Your Record is safe!")
+            }
+        });
+
+
+}
