d2dc0cd55618b290f49eb2ddd23d61478b1e94eb
hamza <hamza66789.mh@gmail.com>
2020-07-15 16:04:50 +0500

    changing in area module

--- MunicipalComplaint/Controllers/AdminPanelController.cs
+++ MunicipalComplaint/Controllers/AdminPanelController.cs
@@ -37,7 +37,7 @@ namespace MunicipalComplaint.Controllers
             cus.createdat = System.DateTime.Today.ToString("dd/MM/yyyy");
             Random pass = new Random();
             int Password = pass.Next(600001, 999998);
-            cus.Password= Convert.ToString(Password);
+            cus.Password = Convert.ToString(Password);
             _context.customer.Add(cus);
             _context.SaveChanges();
             List<CustomerSignup> cs = _context.customer.ToList();
@@ -57,16 +57,16 @@ namespace MunicipalComplaint.Controllers
             {
                 provinces = pv,
                 cities = ct,
-                tehsiles=teh,
-                ucs=uc
-                
+                tehsiles = teh,
+                ucs = uc
+
 
             };
             return View(vm);
         }
-       
+
         [HttpPost]
-            public ActionResult Area(Province province)
+        public ActionResult Area(Province province)
         {
             _context.province.Add(province);
             _context.SaveChanges();
@@ -93,14 +93,14 @@ namespace MunicipalComplaint.Controllers
         public ActionResult AreaUC(UC ucc)
         {
             _context.uc.Add(ucc);
-            int i=_context.SaveChanges();
-             return Json("done");
+            int i = _context.SaveChanges();
+            return Json("done");
 
         }
         [HttpPost]
         public ActionResult drop(int id)
         {
-           List<City>  city= _context.city.Where(x => x.ProvinceId == id).ToList();
+            List<City> city = _context.city.Where(x => x.ProvinceId == id).ToList();
             SelectList cityy = new SelectList(city, "DistrictId", "DistrictName", 0);
 
 
@@ -112,15 +112,16 @@ namespace MunicipalComplaint.Controllers
         public ActionResult dropUC(int id)
         {
             List<Tehsil> teh = _context.tehsil.Where(x => x.DistrictId == id).ToList();
-            SelectList tehsi= new SelectList(teh, "TehsilId", "TehsilName", 0);
+            SelectList tehsi = new SelectList(teh, "TehsilId", "TehsilName", 0);
 
 
             return Json(tehsi);
 
         }
         [HttpPost]
-        public ActionResult chkProForUp(int id) {
-            Province p = _context.province.Single(x=>x.ProvinceId==id);
+        public ActionResult chkProForUp(int id)
+        {
+            Province p = _context.province.Single(x => x.ProvinceId == id);
             return Json(p);
         }
 
@@ -178,30 +179,107 @@ namespace MunicipalComplaint.Controllers
             return Json("done");
         }
         [HttpPost]
-        public ActionResult updateProvince(Province pro) {
+        public ActionResult updateProvince(Province pro)
+        {
 
-          var upPro = _context.province.Single(m => m.ProvinceId== pro.ProvinceId);
+            var upPro = _context.province.Single(m => m.ProvinceId == pro.ProvinceId);
             TryUpdateModel(upPro);
             _context.SaveChanges();
             return Json("done");
         }
         [HttpPost]
 
-        public ActionResult DelProvince(int id) {
-            Province pro = _context.province.SingleOrDefault(x=>x.ProvinceId==id);
-            _context.province.Remove(pro);
+        public ActionResult DelProvince(int id)
+        {
+            var ProDisTehUc = from i in _context.province join j in _context.city on i.ProvinceId equals j.ProvinceId join k in _context.tehsil on j.DistrictId equals k.DistrictId join l in _context.uc on k.TehsilId equals l.TehsilId where i.ProvinceId.Equals(id) select new { mprodis = i, prodis = j, proteh = k, prouc = l };
+            if (ProDisTehUc.Count() == 0)
+            {
+                var DisTehUc = from i in _context.province join j in _context.city on i.ProvinceId equals j.ProvinceId join k in _context.tehsil on j.DistrictId equals k.DistrictId where i.ProvinceId.Equals(id) select new { pro = i, dis = j, teh = k };
+
+                if (DisTehUc.Count() == 0)
+                {
+                    var DisTeh = from i in _context.province join j in _context.city on i.ProvinceId equals j.ProvinceId  where i.ProvinceId.Equals(id) select new { mdis = i, mteh = j };
+                    if (DisTeh.Count() == 0)
+                    {
+                        Province city = _context.province.SingleOrDefault(x => x.ProvinceId== id);
+                        _context.province.Remove(city);
+                    }
+                    else
+                    {
+                        foreach (var mdata in DisTeh)
+                        {
+                            _context.province.Remove(mdata.mdis);
+                            _context.city.Remove(mdata.mteh);
+
+                        }
+                    }
+
+                }
+                else
+                {
+                    foreach (var mydata in DisTehUc)
+                    {
+                        _context.province.Remove(mydata.pro);
+                        _context.city.Remove(mydata.dis);
+                        _context.tehsil.Remove(mydata.teh);
+                    }
+                }
+
+
+               
+            }
+            else
+            {
+                foreach (var delfull in ProDisTehUc) {
+
+                    _context.province.Remove(delfull.mprodis);
+                    _context.city.Remove(delfull.prodis);
+                    _context.tehsil.Remove(delfull.proteh);
+                    _context.uc.Remove(delfull.prouc);
+                }
+
+            }
             _context.SaveChanges();
-                return Json("done");
-          
-           
+            return Json("done");
+
+
         }
 
         [HttpPost]
 
         public ActionResult DelDistrict(int id)
         {
-            City city= _context.city.SingleOrDefault(x => x.DistrictId == id);
-            _context.city.Remove(city);
+            var DisTehUc = from i in _context.city join j in _context.tehsil on i.DistrictId equals j.DistrictId join k in _context.uc on j.TehsilId equals k.TehsilId where i.DistrictId.Equals(id) select new { dis = i, teh = j, ucc = k };
+
+            if (DisTehUc.Count() == 0)
+            {
+                var DisTeh = from i in _context.city join j in _context.tehsil on i.DistrictId equals j.DistrictId where i.DistrictId.Equals(id) select new { mdis = i, mteh = j };
+                if (DisTeh.Count() == 0)
+                {
+                    City city = _context.city.SingleOrDefault(x => x.DistrictId == id);
+                    _context.city.Remove(city);
+                }
+                else
+                {
+                    foreach (var mdata in DisTeh)
+                    {
+                        _context.city.Remove(mdata.mdis);
+                        _context.tehsil.Remove(mdata.mteh);
+
+                    }
+                }
+
+            }
+            else
+            {
+                foreach (var mydata in DisTehUc)
+                {
+                    _context.city.Remove(mydata.dis);
+                    _context.tehsil.Remove(mydata.teh);
+                    _context.uc.Remove(mydata.ucc);
+                }
+            }
+
             _context.SaveChanges();
             return Json("done");
 
@@ -212,8 +290,21 @@ namespace MunicipalComplaint.Controllers
 
         public ActionResult DelTehsil(int id)
         {
-            Tehsil teh= _context.tehsil.SingleOrDefault(x => x.TehsilId == id);
-            _context.tehsil.Remove(teh);
+            var da = from i in _context.tehsil join j in _context.uc on i.TehsilId equals j.TehsilId where i.TehsilId.Equals(id) select new { a = i, b = j };
+            if (da.Count() == 0)
+            {
+                Tehsil teh = _context.tehsil.SingleOrDefault(x => x.TehsilId == id);
+                _context.tehsil.Remove(teh);
+
+            }
+            else
+            {
+                foreach (var a in da)
+                {
+                    _context.tehsil.Remove(a.a);
+                    _context.uc.Remove(a.b);
+                }
+            }
             _context.SaveChanges();
             return Json("done");
 
@@ -224,7 +315,7 @@ namespace MunicipalComplaint.Controllers
 
         public ActionResult DelUc(int id)
         {
-            UC ucc= _context.uc.SingleOrDefault(x => x.UcId == id);
+            UC ucc = _context.uc.SingleOrDefault(x => x.UcId == id);
             _context.uc.Remove(ucc);
             _context.SaveChanges();
             return Json("done");
